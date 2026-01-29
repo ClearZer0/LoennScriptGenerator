d343c092ec41df482aa907a13acf1e5e6615f4e2
ClearZer0 <534310154@qq.com>
2026-01-29 17:11:33 +0800

    Create TestEntity.cs

---
+++ Entities/TestEntity.cs
@@ -0,0 +1,16 @@
+﻿using Celeste.Mod.Entities;
+using Celeste.Mod.LoennScriptGenerator.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.LoennScriptGenerator.Entities;
+
+[CustomEntity("LoennScriptGenerator/Test")]
+public class TestEntity : Entity
+{
+    [PlacementsElement]
+    public int Dash = 1;
+}
